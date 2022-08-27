e1e375c95693b963721c1359a369b2a302257b16
Michael Ng <michaelnch91@gmail.com>
2022-08-27 21:17:33 +0800

    Update scraper redirection HTTP codes

--- lambda_function.rb
+++ lambda_function.rb
@@ -1,4 +1,6 @@
-require 'net/http'
+require "net/http"
+
+REDIRECTION_HTTP_CODES = %w[301 302 307 308].freeze
 
 def lambda_handler(event:, context:)
   # Options within event
@@ -8,27 +10,27 @@ def lambda_handler(event:, context:)
   # }
   url = event.dig("url")
   is_mobile = event.dig("mobile") || false
-  return { code: "400", message: "URL is required" } if url.nil?
+  return {code: "400", message: "URL is required"} if url.nil?
 
   # Begin rescue block to handle invalid URLs
   begin
     response = fetch(url, mobile: is_mobile) # Begin initial fetch
   rescue => e
-    return { code: "400", message: e.message }
+    return {code: "400", message: e.message}
   end
 
   redirection_limit = 10 # Number of redirects allowed
-  is_redirection = response.code == "302"
+  is_redirection = redirection?(response&.code)
 
   while is_redirection && redirection_limit.positive?
     redirection_limit -= 1 # Decrement redirection limit
     response = fetch(response["location"], mobile: is_mobile) # Fetch redirection url until limit is reached
-    is_redirection = response.code == "302"
+    is_redirection = redirection?(response&.code)
   end
 
-  response_code = response.code
+  response_code = response&.code
   # body = response.body.encode!("UTF-8", invalid: :replace, undef: :replace)
-  body = response.body.force_encoding("UTF-8")
+  body = response&.body&.force_encoding("UTF-8")
 
   if !redirection_limit.positive? && response_code == "302"
     response_code = "400" # Set to 400 if redirection limit is reached
@@ -36,7 +38,7 @@ def lambda_handler(event:, context:)
 
   {
     code: response_code,
-    body: body
+    body: body,
   }
 end
 
@@ -55,4 +57,12 @@ def fetch(url, mobile: false)
   Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") { |http|
     http.request(request)
   }
+end
+
+def redirection?(code)
+  # 301 Moved Permanently - https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/301
+  # 302 Found = Temporarily moved to URL given in Location header - https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/302
+  # 307 Temporary Redirect - https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/307
+  # 308 Permanent Redirect - https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/308
+  REDIRECTION_HTTP_CODES.include?(code)
 end
